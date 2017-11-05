576e44046cd1948fa99f35c77e557663d3640735
Matthew Hendrix <mhendrix@athenahealth.com>
2017-11-05 09:12:04 -0500

    all gets

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 const request = require('request-promise-native');
 const express = require('express');
+const storage = require('node-storage');
+const store = new storage('./storage');
 const targetURL = 'https://api.athenahealth.com/preview1';
+const oauthURL = 'https://api.athenahealth.com/oauthpreview/token';
+const key = '';
+const secret = '';
+const keysecret = new Buffer(key + ":" + secret).toString('base64');
 
 module.exports = {
   /**
@@ -25,21 +31,49 @@ module.exports = {
       //support parsing of application/x-www-form-urlencoded post data
       app.use(bodyParser.urlencoded({ extended: true }));
 
+      function passthrough(req, res) {
+        const tokenage = store.get('age');
+        if (typeof tokenage !== 'undefined'
+          && (Date.now() - tokenage) < 3000000
+        ) {
+          makeCall(req, res);
+        }
+        else {
+          const reqOptions = {
+            method: 'POST',
+            uri: oauthURL,
+            form: {
+              grant_type: 'client_credentials',
+            },
+            headers: {
+              Authorization: 'Basic ' + keysecret,
+            },
+            json: true,
+          };
+          request(reqOptions)
+            .then((response) => {
+              store.put('age', Date.now());
+              store.put('token', response.access_token);
 
-      // Hacky, but register our UI routes here...
-      // ------------------ PUBLIC ------------------
-      app.get('/ping', function (req, res) {
+              makeCall(req, res);
+            })
+            .catch((err) => {
+              console.log('died in get');
+              console.log(err);
+            });
+        }
+      }
 
+      function makeCall(req, res) {
         const reqOptions = {
           method: 'GET',
-          uri: targetURL + '/195900/ping',
-          qs: req.query,
+          uri: targetURL + req.originalUrl,
           headers: {
-            Authorization: req.get('Authorization')
-            //Authorization: 'Bearer d3jh7539gndzsgm48qybktmx',
+            Authorization: 'Bearer ' + store.get('token'),
           },
           json: true,
         };
+
         request(reqOptions)
           .then((response) => {
             res.json(response);
@@ -48,6 +82,12 @@ module.exports = {
             console.log('died in get');
             console.log(err);
           });
+      }
+
+      // Hacky, but register our UI routes here...
+      // ------------------ PUBLIC ------------------
+      app.get('/*', function (req, res) {
+        passthrough(req, res);
       });
 
       return resolve(app);
